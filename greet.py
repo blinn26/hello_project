693a77a895e447154332fae4d8498b6a44516b54
createdbybenlinn <ben@benlinn.com>
2023-08-22 10:35:12 -0700

    save changes

--- greet.py
+++ greet.py
@@ -2,8 +2,15 @@ def main():
     name = input("Enter your name: ")
     print(f"Hello, {name}!")
 
+    age = int(input("Enter your age: "))
+
+    if age < 18:
+        print("You're still a minor!")
+    elif 18 <= age < 30:
+        print("You're a young adult!")
+    else:
+        print("You're an adult!")
+
+
 if __name__ == "__main__":
-
     main()
-
-
